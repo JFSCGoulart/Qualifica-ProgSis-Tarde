9cf34fed3854442786ba17c830f97505ac1b693d
just1kiki <kikialg2003@gmail.com>
2026-01-30 17:59:40 -0300

    Atualizar código incorreto
    Permitir que o código faça todo o texto, não só o primeiro bloco.

--- banco.py
+++ banco.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conexao = sqlite3.connect('banco.db')
 cursor = conexao.cursor()
-cursor.execute('''
+cursor.executescript('''
 CREATE TABLE IF NOT EXISTS "usuarios" (
 	"id_usuario" INTEGER PRIMARY KEY AUTOINCREMENT,
 	"nome" VARCHAR(100) NOT NULL,
