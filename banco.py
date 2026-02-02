088fc801eb461069958adf03f4cb785782636f1f
Jonnathan Ribeiro <jrib_st@outlook.com>
2026-02-02 15:55:04 -0300

    fix: ajusta id de cursos e vírgula ausente
    Adiciona INTEGER à chave primária da tabela cursos
    para corrigir o uso do AUTOINCREMENT.
    
    Corrige falta de vírgula nas tabelas usuario_curso
    e usuario_atividade.

--- banco.py
+++ banco.py
@@ -13,14 +13,15 @@ CREATE TABLE IF NOT EXISTS "usuarios" (
 );
 
 CREATE TABLE IF NOT EXISTS "cursos" (
-	"id_curso" PRIMARY KEY AUTOINCREMENT,
+	"id_curso" INTEGER PRIMARY KEY AUTOINCREMENT,
 	"nome" VARCHAR(100) NOT NULL,
 	"horario" INTEGER(1) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "usuario_curso" (
 	"id_usuario" INTEGER NOT NULL,
-	"id_curso" INTEGER NOT NULL	FOREIGN KEY ("id_usuario") REFERENCES "usuarios"("id_usuario")
+	"id_curso" INTEGER NOT NULL,
+	FOREIGN KEY ("id_usuario") REFERENCES "usuarios"("id_usuario")
 	ON UPDATE NO ACTION ON DELETE NO ACTION,
 	FOREIGN KEY ("id_curso") REFERENCES "cursos"("id_curso")
 	ON UPDATE NO ACTION ON DELETE NO ACTION
@@ -43,7 +44,8 @@ CREATE TABLE IF NOT EXISTS "usuario_atividade" (
 	"id_usuario" INTEGER NOT NULL,
 	"id_atividade" INTEGER NOT NULL,
 	"data" DATE NOT NULL,
-	"status" BOOLEAN NOT NULL DEFAULT FALSE	FOREIGN KEY ("id_usuario") REFERENCES "usuarios"("id_usuario")
+	"status" BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY ("id_usuario") REFERENCES "usuarios"("id_usuario")
 	ON UPDATE NO ACTION ON DELETE NO ACTION,
 	FOREIGN KEY ("id_atividade") REFERENCES "atividades"("id_atividade")
 	ON UPDATE NO ACTION ON DELETE NO ACTION
